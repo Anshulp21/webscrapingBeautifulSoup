b59356da102ff46eff8944f879bf109fe64eb100
Anshulp21 <134382165+Anshulp21@users.noreply.github.com>
2024-09-16 02:00:16 +0530

    Update Scrap.py

--- Scrap.py
+++ Scrap.py
@@ -1,117 +1,96 @@
-
-
-# import these two modules bs4 for selecting HTML tags easily
-from bs4 import BeautifulSoup
-# requests module is easy to operate some people use urllib but I prefer this one because it is easy to use.
 import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-
-url="https://getpython.wordpress.com/"
-BASE_URL = "https://getpython.wordpress.com/"
-#Requests module use to data from given url
-source=requests.get(url)
-
-
+# Set up Chrome driver options
 def get_chrome_web_driver(options):
     return webdriver.Chrome("./chromedriver", chrome_options=options)
-
 
 def get_web_driver_options():
     return webdriver.ChromeOptions()
 
-
 def set_ignore_certificate_error(options):
     options.add_argument('--ignore-certificate-errors')
-
 
 def set_browser_as_incognito(options):
     options.add_argument('--incognito')
 
-# BeautifulSoup is used for getting HTML structure from requests response.(craete your soup)
-soup=BeautifulSoup(source.text,'html')
+# Set up pipeline to extract data from webpage
+def extract_data(url):
+    source = requests.get(url)
+    soup = BeautifulSoup(source.text, 'html.parser')
+    
+    # Extract title
+    title = soup.find('title').text
+    
+    # Extract all links
+    links = soup.find_all('a', href=True)
+    
+    # Extract all images
+    images = soup.find_all('img')
+    
+    # Extract all tables with class 'infobox vevent'
+    tables = soup.find_all('table', class_='infobox vevent')
+    
+    return {
+        'title': title,
+        'links': links,
+        'images': images,
+        'tables': tables
+    }
 
-# Find function is used to find a single element if there are more than once it always returns the first element.
-title=soup.find('title') # place your html tagg in parentheses that you want to find from html.
-print("this is with html tags :",title)
+# Set up pipeline to extract data from Wikipedia
+def extract_wikipedia_data(url):
+    source = requests.get(url)
+    soup = BeautifulSoup(source.text, 'html.parser')
+    
+    # Extract title
+    title = soup.find('title').text
+    
+    # Extract all tables with class 'infobox vevent'
+    tables = soup.find_all('table', class_='infobox vevent')
+    
+    # Extract all divs with class 'toc'
+    toc = soup.find_all('div', class_='toc')
+    
+    return {
+        'title': title,
+        'tables': tables,
+        'toc': toc
+    }
 
-qwery=soup.find('h1') # here i find first h1 tagg in my website using find operation.
+# Main function
+def main():
+    url = "https://getpython.wordpress.com/"
+    wikipedia_url = "https://en.wikipedia.org/wiki/World_War_II"
+    
+    # Extract data from webpage
+    data = extract_data(url)
+    
+    # Extract data from Wikipedia
+    wikipedia_data = extract_wikipedia_data(wikipedia_url)
+    
+    # Print extracted data
+    print("Title:", data['title'])
+    print("Links:")
+    for link in data['links']:
+        print(link['href'])
+    print("Images:")
+    for image in data['images']:
+        print(image)
+    print("Tables:")
+    for table in data['tables']:
+        print(table.text)
+    
+    print("\nWikipedia Data:")
+    print("Title:", wikipedia_data['title'])
+    print("Tables:")
+    for table in wikipedia_data['tables']:
+        print(table.text)
+    print("TOC:")
+    for toc in wikipedia_data['toc']:
+        print(toc.text)
 
-#use .text for extract only text without any html tags
-print("this is without html tags:",qwery.text) 
-
-
-links=soup.find('a') #i extarcted link using "a" tag
-print(links)
-
-
-# ## extarct data from innerhtml 
-
-# here i extarcted href data from anchor tag.
-print(links['href']) 
-
-##  or another way
-##extracting href(links) attribute and anchor(<a>) tag from page 
-for a in soup.find_all('a', href=True):
-    print ( a['href'])
-
-for i in links:
-    print(i.text)
-
-# similarly i got class details from a anchor tag
-print(links['class'])
-
-
-# ## findall operation in Bs4
-
-# findall function is used to fetch all tags at a single time.
-many_link=soup.find_all('a') # here i extracted all the anchor tags of my website
-total_links=len(many_link) # len function is use to calculate length of your array
-print("total links in my website :",total_links)
-print()
-for i in many_link[:6]: # here i use slicing to fetch only first 6 links from rest of them.
-    print(i)
-
-second_link=many_link[1] #here i fetch second link which place on 1 index number in many_links.
-print(second_link)
-print()
-print("href is :",second_link['href']) #only href link is extracted from ancor tag
-
-
-# select div tag from second link
-nested_div=second_link.find('div')
-# As you can see div element extarcted , it also have inner elements
-print(nested_div)
-print()
-#here i extracted class element from div but it give us in the form of list
-z=(nested_div['class'])
-print(z)
-print(type(z))
-print()
-#  " " .join () method use to convert list type  into string type
-print("class name of div is :"," ".join(nested_div['class'])) 
-
-
-# ## scrap data from wikipedia
-
-wiki=requests.get("https://en.wikipedia.org/wiki/World_War_II")
-soup=BeautifulSoup(wiki.text,'html')
-print(soup.find('title'))
-
-
-# ### find html tags with classes
-
-ww2_contents=soup.find_all("div",class_='toc')
-for i in ww2_contents:
-    print(i.text)
-
-
-overview=soup.find_all('table',class_='infobox vevent')
-for z in overview:
-    print(z.text)
-  
-images=soup.find_all('img')
-
-images
-##or
-print(images)
+if __name__ == "__main__":
+    main()
